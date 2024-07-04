8c41355a2d13ba911117eb6816e0b31dfc8279f8
MoseNjau <moses.njau@students.jkuat.ac.ke>
2024-07-05 00:07:54 +0300

    Main Layout

---
+++ src/layouts/MainLayout.js
@@ -0,0 +1,21 @@
+// src/layouts/MainLayout.js
+import React from 'react';
+import { Box, Flex } from '@chakra-ui/react';
+import Sidebar from '../components/Common/Sidebar';
+import Navbar from '../components/Common/Navbar';
+import Footer from '../components/Common/Footer';
+
+const MainLayout = ({ children }) => (
+  <Flex>
+    <Sidebar />
+    <Box flex="1" ml={{ base: 0, md: 60 }} p="4">
+      <Navbar />
+      <Box as="main" p="4">
+        {children}
+      </Box>
+      <Footer />
+    </Box>
+  </Flex>
+);
+
+export default MainLayout;
