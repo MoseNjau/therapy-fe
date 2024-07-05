5a0aecb18f3a7063284144e35f7b619c3c6c3452
MoseNjau <moses.njau@students.jkuat.ac.ke>
2024-07-05 08:43:33 +0300

    Added 404 page

---
+++ src/pages/NotFound.js
@@ -0,0 +1,14 @@
+// src/pages/NotFound.js
+import React from 'react';
+import { Box, Heading } from '@chakra-ui/react';
+
+const NotFound = () => {
+  return (
+    <Box>
+      <Heading>404 - Page Not Found</Heading>
+      {/* Add your 404 content here */}
+    </Box>
+  );
+};
+
+export default NotFound;
