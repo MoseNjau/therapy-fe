beb7e90f43aa79706f26aed26e808690a8b8f10c
MoseNjau <moses.njau@students.jkuat.ac.ke>
2024-07-09 19:51:59 +0300

    updated dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,5 @@
-// Dashboard.js
+// pages/Dashboard.js
 import React from "react";
-import MainLayout from "../layouts/MainLayout";
 import DashboardContent from "../components/Dashboard/DashboardContent";
 
 const Dashboard = () => {
